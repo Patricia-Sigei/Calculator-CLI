8a15fdbceab55839417ba3e9373afe67d7880cfa
Patricia-Sigei <pattychela97@gmail.com>
2025-01-20 20:49:42 +0300

    improves the calculation logic

--- main.py
+++ main.py
@@ -1,35 +1,92 @@
 # Calculator menu
-print ("Select an Operation to Perform:")
-print ("1. Addition")
-print ("2. Subtraction")
-print ("3. Multiplication")
-print ("4. Division")
+# print ("Select an Operation to Perform:")
+# print ("1. Addition")
+# print ("2. Subtraction")
+# print ("3. Multiplication")
+# print ("4. Division")
 
-# Operation logic
-def calculate ():
-    operation = input ('Enter the number of operation from 1-4:')
+# class Operation:
+#     def _init_ (self, addition, subtraction, multiplication, division):
+#             self.addition = addition
+#             self.subtraction = subtraction
+#             self.multiplication = multiplication
+#             self.division = division
+
+# # Operations = [
+# {"addition" : "-"},
+# {"subtraction" : "-"},
+# {"multiplication" : "*"},
+# {"division" : "/"},
+# {"exponent" : "**"}
+# ]
+
+# def calculate():
+#     operations = ["parenthesis", "exponent", "multiply", "divide", "addition", "subtraction"]
     
-    num_1 = int(input('please enter the first number:'))
-    num_2 = int(input('input the second number:'))
+#     while True:
+#         operation = input("Enter Operation")
+    
+#         numbers = []
+#         while True:
+#                 num = int(input("Enter a number"))
+#                 numbers.append(num)
+        
+#         if operation == "addition":
+#             result = sum(numbers)
+#         elif operation == "subtraction":
+#             result = numbers[0]
+#             for num in numbers[1:]:
+#                 result -= num
+#         elif operation == "multiply":
+#             result = 1
+#             for num in numbers:
+#                 result *= num
+#         elif operation == "divide":
+#             result = numbers[0]
+#             for num in numbers[1:]:
+#                 result /= num
+#         elif operation == "exponent":
+#             result = numbers[0]
+#             for num in numbers[1:]:
+#                 result **= num
+
+
+# calculate()
+
+
+
+
+# # Operation logic
+def calculate():
+    operation = input('Enter the number of operation (1 for addition, 2 for subtraction, 3 for multiplication, 4 for division): ')
+    numbers = input('Enter the numbers separated by spaces: ').split()
+    numbers = [float(num) for num in numbers]
 
     if operation == '1':
-        print ('{} + {} = '.format(num_1, num_2))
-        print(num_1 + num_2)
+        result = sum(numbers)
+        print(f"The result of adding {numbers} is: {result}")
     elif operation == '2':
-        print ('{} - {} = '.format(num_1, num_2))
-        print (num_1 - num_2)
+        result = numbers[0]
+        for num in numbers[1:]:
+            result -= num
+        print(f"The result of subtracting {numbers} is: {result}")
     elif operation == '3':
-        print ('{} * {} ='.format(num_1, num_2))
-        print (num_1 * num_2)
+        result = 1
+        for num in numbers:
+            result *= num
+        print(f"The result of multiplying {numbers} is: {result}")
     elif operation == '4':
-        if num_2 == 0:
-            print ('Division by 0 is not allowed')
-        else:
-            print ('{} / {} = '.format(num_1, num_2))
-            print (num_1 / num_2)
+        result = numbers[0]
+        try:
+            for num in numbers[1:]:
+                result /= num
+            print(f"The result of dividing {numbers} is: {result}")
+        except ZeroDivisionError:
+            print("Error: Division by zero is not allowed.")
     else:
-        print ('Please type a valid operation')
+        print('Please type a valid operation')
 
-calculate ()
+calculate()
+
 
 
